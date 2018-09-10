4aa731574e4a4bac1bd31459bf22f4b89f9deda1
kayla <1286934653@qq.com>
2018-09-10 09:33:20 +0800

    练手项目

---
+++ center/js/help.js/menu.js
@@ -0,0 +1,12 @@
+var menu = new Vue({
+    el:"",
+    data:{
+        menu_List:[{
+             "menu_List1":"入门指南",
+             "menu_List2":"应用创建指南",
+             "menu_List3":"服务使用指南",
+             "menu_List4":"常见问题"
+        }
+        ]
+    }
+})
